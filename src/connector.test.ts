0b5e8dae9d4a44f71fdf74d70ede70029c353220
chan-shiro <shiro.fkd@gmail.com>
2024-10-03 12:46:08 +0900

    add test template

---
+++ src/connector.test.ts
@@ -0,0 +1,30 @@
+// jest.mock("../collections/integration-collection");
+// const mockedIntegrationCollection = jest.mocked(IntegrationCollection);
+
+import * as zip from "./connector";
+
+describe("Zip", () => {
+  describe("getAddress", () => {
+    describe("when zip=7830060", () => {
+      it("returns ZipResponse as JSON with Kochi Prefecture", async () => {
+        const response = await zip.getAddress("7830060");
+        expect(response).toEqual({
+          message: null,
+          results: [
+            {
+              address1: "高知県",
+              address2: "南国市",
+              address3: "蛍が丘",
+              kana1: "ｺｳﾁｹﾝ",
+              kana2: "ﾅﾝｺｸｼ",
+              kana3: "ﾎﾀﾙｶﾞｵｶ",
+              prefcode: "39",
+              zipcode: "7830060"
+            }
+          ],
+          status: 200
+        });
+      });
+    });
+  });
+});
--- src/connector.ts
+++ src/connector.ts
@@ -1,12 +1,29 @@
-function greeting(name: string, reply: number, text: string, suff: string): string {
-  const returnString = 'Hello World!' + name + reply + text + suff;
-  console.log(returnString);
-  console.log('hage');
-  return returnString;
-};
+import fetch from 'node-fetch';
 
-const hello = {
-  greeting: greeting
+type ZipResponse = {
+  message: null | string;
+  results: {
+    address1: string;
+    address2: string;
+    address3: string;
+    kana1: string;
+    kana2: string;
+    kana3: string;
+    prefcode: string;
+    zipcode: string;
+  }[];
+  status: number;
 }
 
-export = hello;
+async function getAddress(zip: string): Promise<ZipResponse> {
+  const url = `https://zipcloud.ibsnet.co.jp/api/search?zipcode=${zip}`;
+  const response = await fetch(url);
+  const json = await response.json() as ZipResponse;
+  return json;
+}
+
+const zip = {
+  getAddress
+};
+
+export = zip;
